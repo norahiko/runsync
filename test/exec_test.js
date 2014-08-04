e9dfd4a8e3832e766d49b50fe60b75c2632f3b6a
norahiko <norahiko.o@gmail.com>
2014-08-04 23:10:22 +0900

    Add exec test

---
+++ test/exec_test.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var runsync = require("../index.js");
+var chai = require("chai");
+
+var assert = chai.assert;
+var equal = assert.strictEqual;
+
+suite("exec", function() {
+    var expected;
+    var err = process.stderr.write;
+
+    setup(function () {
+        process.stderr.write = function(str) {
+            expected = str;
+        };
+    });
+
+    teardown(function () {
+        process.stderr.write = err;
+    });
+
+
+    test("exec ok", function() {
+        var res = runsync.exec("echo ok");
+        equal(res.toString(), "ok\n");
+    });
+
+    test("exec fail", function() {
+        assert.throws(function () {
+            runsync.exec("echo error 1>&2 && exit 1");
+        }, "Command failed");
+
+        equal(expected, "error\n");
+    });
+
+    test("throw Error", function() {
+        assert.throws(function () {
+            runsync.exec("node -e \"throw new Error('exec error');\"");
+        }, "Command failed");
+
+        assert(/exec error/.test(expected));
+    });
+});
